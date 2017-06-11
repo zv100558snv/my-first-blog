36570c169a93a5f8f6b2b311b8cbecdf4268b5d5
zv100558snv <zv100558snv@gmail.com>
2017-06-11 18:53:03 +0300

    Modified commit006

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,4 @@ def post_list(request):
 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     
-    return render(request, 'blog/post_list.html', {'posts': posts})
-    
+    return render(request, 'blog/post_list.html', {'posts': posts}) 
